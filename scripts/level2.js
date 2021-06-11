5e2463fa7510cd4a714ca456e1e96fec2ddb1160
Lovisa Elensky <70098425+lovisaelensky@users.noreply.github.com>
2021-06-11 10:25:25 +0200

    Add files via upload

--- scripts/level2.js
+++ scripts/level2.js
@@ -1,6 +1,10 @@
 const introModalTwo = document.querySelector('.modal-intro-two');
 const closeBtn = document.querySelector('.close-btn');
+const bgMusic = new Audio(
+  'assets/i-have-sinned-creepy-mysterious-free-music-2466.mp3'
+);
 
 closeBtn.addEventListener('click', function () {
   introModalTwo.classList.remove('bg-active');
+  bgMusic.play();
 });
--- scripts/level3.js
+++ scripts/level3.js
@@ -1,9 +1,13 @@
 const introModalThree = document.querySelector('.modal-intro-three');
 const closeBtn = document.querySelector('.close-btn');
 const exit = document.querySelector('.exit');
+const bgMusic = new Audio(
+  'assets/i-have-sinned-creepy-mysterious-free-music-2466.mp3'
+);
 
 closeBtn.addEventListener('click', function () {
   introModalThree.classList.remove('bg-active');
+  bgMusic.play();
 });
 
 exit.addEventListener('click', function () {
